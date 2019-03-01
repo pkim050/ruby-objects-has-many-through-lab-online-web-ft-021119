c391de0d2d7c499f27ad5134714baf117fb4d310
Patrick Kim <pkim050@gmail.com>
2019-03-01 20:05:10 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,8 +11,8 @@ class Doctor
     @@all
   end
   
-  def new_appointment()
-    
+  def new_appointment(date, patient)
+    doctor = Appointment.new(date, patient, self)
   end
   
   def appointments
